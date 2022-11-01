4ab4f37dc4dda342d693355c8f812a5f2157db03
litcoderr <litcoderr@gmail.com>
2022-11-01 14:28:42 +0900

    added docker

---
+++ launch.sh
@@ -0,0 +1,8 @@
+CUDA_VISIBLE_DEVICES='2,5'
+
+docker run --gpus '"'device=$CUDA_VISIBLE_DEVICES'"' --ipc=host --rm -it \
+    --mount src=$(pwd),dst=/CLIP4Clip,type=bind \
+    -e NVIDIA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
+    -w /CLIP4Clip \
+    litcoderr/clip4clip:latest \
+    bash -c "bash" \
